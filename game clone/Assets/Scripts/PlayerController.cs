9c47e47d11e20231fbe6a50f391a9b22d3a47f69
jewelypoo <jewelxjocelyn@gmail.com>
2023-10-24 09:03:07 -0700

    player controller script

---
+++ game clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * [Fain, Jewel]
+ * [10/24/2023]
+ * [handles all player movement]
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
