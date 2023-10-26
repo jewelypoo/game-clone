b33ab785208eb2c93521a90ee805a13948257b0f
hjgibson <rdohed2@gmail.com>
2023-10-26 08:50:08 -0700

    working on player movement

--- game clone/Assets/Scripts/Rotate.cs
+++ game clone/Assets/Scripts/Rotate.cs
@@ -16,12 +16,12 @@ public class Rotate : MonoBehaviour
     {
         // if it is facing right then u can press key to face other direction, otherwise it wont turn
         // look at harvester
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.Rotate(Vector2.up * 180);
-        }
+     ///  if (Input.GetKeyDown(KeyCode.Space))
+       /// {
+       ///     transform.Rotate(Vector2.up * 180);
+       /// }
         
-        ///will make it move depending on which way it is facing
+        //will make it move depending on which way it is facing
        
        
     }
