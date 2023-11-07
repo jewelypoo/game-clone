74043fb7bbf4a397edc0be19445ea9ce5c7d89d4
jewelypoo <jewelxjocelyn@gmail.com>
2023-11-07 10:43:54 -0800

    disappearing platforms

---
+++ game clone/Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * [Fain, Jewel / Gibson, Hannah]
+ * [11/7/2023]
+ * [handles all disappearing platforms]
+ */
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public float togglePlatformTime = 2;
+    public float currentTime = 0;
+    public bool enabled = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= togglePlatformTime)
+        {
+            currentTime = 0;
+            TogglePlatform();
+        }
+    }
+    void TogglePlatform()
+    {
+        enabled = !enabled;
+        foreach (Transform child in gameObject.transform)
+        {
+            if (child.tag != "Player")
+            {
+                child.gameObject.SetActive(enabled);
+            }
+        }
+    }
+}
